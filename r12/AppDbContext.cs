eb079928ddb67d1e82c3dd9d879e8691225330ae
daighj2 <artem.dyatlov2005@bk.ru>
2023-04-12 21:47:09 +0300

    добавил формы, и кнопку регистрации

---
+++ r12/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r12
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=LibraryDb;Integrated Security=True;";
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tovari> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Books)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId);
+        }
+    }
+}
---
+++ r12/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r12
+{
+        public class User
+    {
+        
+        public string Name { get; set; }
+        public string email { get; set; }
+        public string number { get; set; }
+    }
+    public class Tovari
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Klient { get; set; }
+        public string Tovar { get; set; }
+        public string kolichestvo { get; set; }
+    }
+}
